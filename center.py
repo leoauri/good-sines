303e250e5a7f922c9e95630adc779463c3f6f026
Leo Auri <code@leoauri.com>
2021-06-19 22:08:19 +0200

    Test sine contains something

--- center.py
+++ center.py
@@ -1,2 +1,7 @@
 import pytorch_lightning as pl
 
+class GoodSines(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+
+    
--- test_audible_sines.py
+++ test_audible_sines.py
@@ -4,3 +4,7 @@ sine_pool = SinePool()
 
 def test_epoch_size():
     assert len(list(iter(sine_pool))) == sine_pool.epoch_size
+
+def test_contains_something():
+    sine = sine_pool.random_sine()
+    assert sine.max() != sine.min()
