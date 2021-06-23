86ff21be8ee2c030baf652263662b8e9058f0857
Leo Auri <code@leoauri.com>
2021-06-23 15:54:12 +0200

    Make volume stochastic

--- audible_sines.py
+++ audible_sines.py
@@ -6,7 +6,7 @@ import random
 
 class SinePool(torch.utils.data.IterableDataset):
     def __init__(self, epoch_size=100, sr=44100, sample_len=2, fade_len=0.2, min_freq=20, 
-            max_freq=20000):
+            max_freq=20000, min_volume=-40, max_volume=0):
         super(SinePool).__init__()
         self.epoch_size = epoch_size
         self.sr = sr
@@ -18,6 +18,8 @@ class SinePool(torch.utils.data.IterableDataset):
             np.linspace(1,0,fade_samples)))
         self.min_freq = min_freq
         self.max_freq = max_freq
+        self.min_volume = min_volume
+        self.max_volume = max_volume
 
         assert len(self.window) == self.sample_len
 
@@ -25,9 +27,14 @@ class SinePool(torch.utils.data.IterableDataset):
         for i in range(self.epoch_size):
             yield self.random_sine()
 
+    @staticmethod
+    def db_to_amp(db):
+        return 10**(db/20)
+
     def random_sine(self):
         freq = random.uniform(self.min_freq, self.max_freq)
-        tone = librosa.tone(freq, sr=self.sr, length=self.sample_len)
+        amp = self.db_to_amp(random.uniform(self.min_volume, self.max_volume))
+        tone = librosa.tone(freq, sr=self.sr, length=self.sample_len) * amp
         return tone * self.window
 
 class AudibleSines(pl.LightningDataModule):
--- test_audible_sines.py
+++ test_audible_sines.py
@@ -1,4 +1,5 @@
 from audible_sines import SinePool
+import math
 
 sine_pool = SinePool()
 
@@ -8,3 +9,18 @@ def test_epoch_size():
 def test_contains_something():
     sine = sine_pool.random_sine()
     assert sine.max() != sine.min()
+
+def amp_to_db(amp):
+    return 20 * math.log(amp, 10)
+
+def test_volumes():
+    for vol in [-30, -20, -12, -6, -3, 0]:
+        sine_pool_volume = SinePool(min_volume=vol, max_volume=vol)
+        sine = sine_pool_volume.random_sine()
+        assert amp_to_db(sine.max()) - vol < 1e-8
+
+def test_volume_stochastic():
+    sine_pool_volume = SinePool(min_volume=-6, max_volume=-3)
+    sine = sine_pool_volume.random_sine()
+    assert amp_to_db(sine.max()) < -3 + 1e-8
+    assert amp_to_db(sine.max()) > -6 - 1e-8
